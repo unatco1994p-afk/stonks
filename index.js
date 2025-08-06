b291d2a1b3c4acdd2a52c946bba4a2200f5c8b9f
Tomasz Zwierzyński <tomasz0zwierzynski@gmail.com>
2025-08-06 20:22:51 +0200

    Added firebase connection

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const app = express();
 
+const admin = require('firebase-admin');
+
 const PORT = process.env.PORT || 8080;
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(process.env.FIRESTORE_CREDENTIALS)
+  });
+}
+
+const db = admin.firestore();
 
 app.get('/', (req, res) => {
   res.send('Hello World from Node.js on Google Cloud!');
+});
+
+app.get('/log', async (req, res) => {
+  const ref = await db.collection('testMessages').add({
+    createdAt: new Date()
+  });
+
+  res.send('Returned: ' + ref.id);
 });
 
 app.listen(PORT, () => {

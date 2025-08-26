614c4b6261a8b22f3041c697ec238a8f076b534e
Tomasz Zwierzyński <tomasz0zwierzynski@gmail.com>
2025-08-26 18:34:42 +0200

    Handle dates

--- config/db.js
+++ config/db.js
@@ -1,7 +1,22 @@
-import { Firestore } from '@google-cloud/firestore';
+import { Firestore, Timestamp } from '@google-cloud/firestore';
 import { initializeApp, cert } from 'firebase-admin/app';
 
 const credentials = JSON.parse(process.env.FIRESTORE_CREDENTIALS);
+
+/**
+ * Uniwersalny konwerter Firestore → ISO stringi dla Timestamp
+ */
+const globalConverter = {
+  toFirestore: data => data,
+
+  fromFirestore(
+    snapshot,
+    options
+  ) {
+    const data = snapshot.data(options);
+    return convertTimestamps(data);
+  }
+};
 
 initializeApp({
   credential: cert(credentials),
@@ -17,8 +32,26 @@ const db = new Firestore({
   databaseId: process.env.FIRESTORE_DB
 });
 
-export const USERS_COLLECTION = db.collection('users');
-export const INVESTMENT_COLLECTION = db.collection('testInvestments');
-export const LOGS_COLLECTION = db.collection('testMessages');
+export const USERS_COLLECTION = db.collection('users').withConverter(globalConverter);
+export const INVESTMENT_COLLECTION = db.collection('testInvestments').withConverter(globalConverter);
+export const LOGS_COLLECTION = db.collection('testMessages').withConverter(globalConverter);
 
 export default db;
+
+/**
+ * Rekurencyjnie zamienia Timestamp na ISO string
+ */
+function convertTimestamps(obj) {
+  if (obj instanceof Timestamp) {
+    return obj.toDate().toISOString();
+  } else if (Array.isArray(obj)) {
+    return obj.map(v => convertTimestamps(v));
+  } else if (obj !== null && typeof obj === "object") {
+    const result = {};
+    Object.entries(obj).forEach(([key, value]) => {
+      result[key] = convertTimestamps(value);
+    });
+    return result;
+  }
+  return obj;
+}
